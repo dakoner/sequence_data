61cf2fee4725e6659ac399054daad85c381ed5dd
David Konerding <dakoner@gmail.comwq>
2023-11-03 10:39:57 -0700

    Reduce size of kmer

--- generate_seqs.py
+++ generate_seqs.py
@@ -3,10 +3,10 @@ import pickle
 
 bases = ["A", "C", "G", "T"]
 n = 10000000
-k = 8
+k = 4
 
-x = choices(bases, k=n * 8)
-chunk_size = 8
+x = choices(bases, k=n * k)
+chunk_size = k
 sequences = [
     "".join(x[i : i + chunk_size]) for i in range(0, len(x), chunk_size)
 ]
